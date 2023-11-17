a8ffa526833e090e7d93db6494fd7d348d8c4629
GMwanda <George.Mwanda@strathmore.edu>
2023-11-17 13:36:33 +0300

    Ball and paths with user input enabled

--- BounceProperties.py
+++ BounceProperties.py
@@ -1,24 +1,86 @@
 import pygame
 
 pygame.init()
-win = pygame.display.set_mode((800, 600))  # Set the width and height of your window
+
+# GAME WINDOW
+WIDTH, HEIGHT = 800, 600
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Bounce Maze Game")
 
 
 class Ball:
     def __init__(self):
+        self.x = WIDTH // 2
+        self.y = HEIGHT // 2
+        self.radius = 10
+        self.color = pygame.Color('black')
+        self.velocity = [0, 0]  # Adjust as needed
 
-    # Define the initial properties of the ball here
-
-    def update(self):
-
-
-# Update the position and behavior of the ball here
+    def draw(self):
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
 
 
-class Brick:
+class Maze:
     def __init__(self):
+        self.walls = [
+            pygame.Rect(100, 100, 200, 20),
+            pygame.Rect(300, 200, 20, 200),
+            pygame.Rect(100, 300, 200, 20),
+            pygame.Rect(400, 100, 20, 200),
+        ]
 
-    # Define the initial properties of the brick here
+    def draw(self):
+        for wall in self.walls:
+            pygame.draw.rect(win, pygame.Color('blue'), wall)
 
-    def update(self):
-# Update the position and behavior of the brick here
+
+def check_collision(ball, maze):
+    for wall in maze.walls:
+        if pygame.Rect(ball.x - ball.radius, ball.y - ball.radius, ball.radius * 2, ball.radius * 2).colliderect(wall):
+            ball.velocity[0] *= -1
+            ball.velocity[1] *= -1
+
+
+running = True
+clock = pygame.time.Clock()
+
+ball = Ball()
+maze = Maze()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        # Handle user input
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                ball.velocity[0] = -2
+            elif event.key == pygame.K_RIGHT:
+                ball.velocity[0] = 2
+            elif event.key == pygame.K_UP:
+                ball.velocity[1] = -2
+            elif event.key == pygame.K_DOWN:
+                ball.velocity[1] = 2
+
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                ball.velocity[0] = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                ball.velocity[1] = 0
+
+    # Update ball position based on velocity
+    ball.x += ball.velocity[0]
+    ball.y += ball.velocity[1]
+
+    check_collision(ball, maze)
+    # Draw everything
+    win.fill(pygame.Color('white'))
+    maze.draw()
+    ball.draw()
+
+    pygame.display.flip()
+
+    clock.tick(60)  # Adjust the frame rate as needed
+
+pygame.quit()
