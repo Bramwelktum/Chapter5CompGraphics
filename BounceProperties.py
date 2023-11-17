f919f79e0fdbc032964666dcf9766c0575656d19
GMwanda <George.Mwanda@strathmore.edu>
2023-11-17 09:28:51 +0300

    Project Skeleton

---
+++ BounceProperties.py
@@ -0,0 +1,24 @@
+import pygame
+
+pygame.init()
+win = pygame.display.set_mode((800, 600))  # Set the width and height of your window
+
+
+class Ball:
+    def __init__(self):
+
+    # Define the initial properties of the ball here
+
+    def update(self):
+
+
+# Update the position and behavior of the ball here
+
+
+class Brick:
+    def __init__(self):
+
+    # Define the initial properties of the brick here
+
+    def update(self):
+# Update the position and behavior of the brick here
